7122bac0e807eb6f69d3014c2f9f3ceac00bad48
Mania124 <shikukuhezborn@gmail.com>
2025-06-17 16:24:25 +0300

    add: boilerplate code for react practise

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Note from './components/Note'
+
+const App = (props) => {
+  const { notes } = props
+
+  return (
+    <>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+    </>
+  )
+}
+
+export default App
