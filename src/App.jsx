9e93aa1fc4598a1ec48ab7c83bb3c75b6a6f398a
Mania124 <shikukuhezborn@gmail.com>
2025-08-04 21:39:22 +0300

    add: toggle importance of notes

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,16 @@ const App = () => {
   useEffect(hook, [])
   console.log('render', notes.length, 'notes')
 
+    const toggleImportanceOf = id => {
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    axios.put(url, changedNote).then(response => {
+      setNotes(notes.map(note => note.id === id ? response.data : note))
+    })
+  }
+
   const addNote = event => {
   event.preventDefault()
   const noteObject = {
@@ -50,7 +60,11 @@ const App = () => {
       </div>
       <ul>
         {notesToShow.map(note => 
-          <Note key={note.id} note={note} />
+          <Note 
+          key={note.id} 
+          note={note} 
+          toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         )}
       </ul>
       <form onSubmit={addNote}>
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,6 +1,13 @@
-const Note = ({ note }) => {
-    // console.log('Note component rendered with note:', note.id)
-    return <li>{note.content}</li>
+const Note = ({ note, toggleImportance }) => {
+  const label = note.important
+    ? 'make not important' : 'make important';
+
+  return (
+    <li>
+      {note.content} 
+      <button onClick={toggleImportance}>{label}</button>
+    </li>
+  )
 }
 
 export default Note
