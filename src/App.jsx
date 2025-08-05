b9d05bba888da894222e514cb175f4408ef1560b
Mania124 <shikukuhezborn@gmail.com>
2025-08-05 21:22:30 +0300

    add styles for component elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import Note from './components/Note'
-// import axios from 'axios'
+import Notification from './components/Message'
 import noteService from './services/notes'
 
 
@@ -8,6 +8,7 @@ const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState('some error happened...')
 
   const hook = () => {
   console.log('effect')
@@ -20,23 +21,24 @@ const App = () => {
   useEffect(hook, [])
   console.log(`render ${notes.length} notes`)
 
-    const toggleImportanceOf = (id) => {
-    const url = `http://localhost:3001/notes/${id}`
+  const toggleImportanceOf = id => {
     const note = notes.find(n => n.id === id)
     const changedNote = { ...note, important: !note.important }
 
     noteService
-    .update(url, changedNote)
-    .then(returnedNote => {
-      setNotes(notes.map(note => note.id === id ? returnedNote : note))
-    })
-    .catch(error => {
-      alert(
-        `the note ${note.content} was already deleted from the server`
-      )
-      console.log(error)
-      setNotes(notes.filter(n => n.id != id))
-    })
+      .update(id, changedNote).then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+      .catch(error => {
+
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+        setNotes(notes.filter(n => n.id !== id))
+      })
   }
   const addNote = event => {
   event.preventDefault()
@@ -62,6 +64,7 @@ const App = () => {
   return (
     <>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
